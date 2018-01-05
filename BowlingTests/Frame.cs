469c001a1232e7c2598ad7ec9aa7f187fec3aeee
gothandy <gothandy@hotmail.com>
2018-01-05 22:10:04 +0000

    Refactored Frame class slightly.

--- BowlingTests/Frame.cs
+++ BowlingTests/Frame.cs
@@ -11,16 +11,29 @@ namespace BowlingTests
         {
             throws = new int[frameScoreSheet.Length];
 
+            ConvertFrameScoreSheetStringToThrows(frameScoreSheet);
+        }
+
+        private void ConvertFrameScoreSheetStringToThrows(string frameScoreSheet)
+        {
             for (int i = 0; i < frameScoreSheet.Length; i++)
             {
                 throws[i] = getNumberOfThrowsFromScoreCardChar(frameScoreSheet[i]);
             }
+
+            WhenSpareCalculateSecondThrow();
+        }
+
+        private void WhenSpareCalculateSecondThrow()
+        {
+            if (throws.Length > 1 && throws[1] == -1) throws[1] = 10 - throws[0];
         }
 
         private int getNumberOfThrowsFromScoreCardChar(char c)
         {
             if (char.IsNumber(c)) return Convert.ToInt32(c.ToString());
-
+            if (c == 'X') return 10;
+            if (c == '/') return -1;
             return 0;
         }
 
--- BowlingTests/FrameTests.cs
+++ BowlingTests/FrameTests.cs
@@ -5,12 +5,16 @@ namespace BowlingTests
     public class FrameTests
     {
         [Theory]
-        [InlineData("--", new int[]{0, 0})]
-        [InlineData("11", new int[] { 1, 1 })]
-        [InlineData("81", new int[] { 8, 1 })]
-        [InlineData("34", new int[] { 3, 4 })]
-        [InlineData("72", new int[] { 7, 2 })]
-        public void TwoMisses (string scoreCard, int[] expectedThrows)
+        [InlineData("--", 0, 0)]
+        [InlineData("11", 1, 1)]
+        [InlineData("81", 8, 1)]
+        [InlineData("34", 3, 4)]
+        [InlineData("72", 7, 2)]
+        [InlineData("4/", 4, 6)]
+        [InlineData("4/4", 4, 6, 4)]
+        [InlineData("X", 10)]
+        [InlineData("XXX", 10, 10, 10)]
+        public void TwoMisses (string scoreCard, params int[] expectedThrows)
         {
             Frame frame = new Frame(scoreCard);
 
