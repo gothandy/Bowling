04daa656eaf9b328eb2128a633bbd73049fb4603
gothandy <gothandy@hotmail.com>
2018-01-06 16:57:13 +0000

    More score card tests.

--- BowlingTests/ScoreSheetTests.cs
+++ BowlingTests/ScoreSheetTests.cs
@@ -12,6 +12,10 @@ namespace BowlingTests
         [InlineData("2F", 2, 0)]
         [InlineData("1/", 1, 9)]
         [InlineData("F/", 0, 10)]
+        [InlineData("X", 10)]
+        [InlineData("XXX", 10, 10, 10)]
+        [InlineData("5/5", 5, 5, 5)]
+        [InlineData("5/X", 5, 5, 10)]
         public void GetPinsKnockedDown(string frame, params int[] expected)
         {
             int[] actual = ScoreSheet.GetPinsKnockedDown(frame);
