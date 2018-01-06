600dd79a5c80c05ad7bb860fff2aa4828b3312bf
gothandy <gothandy@hotmail.com>
2018-01-06 13:02:31 +0000

    Adding constants where possible.

---
+++ Bowling/ScoreSheet.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Bowling
+{
+    public class ScoreSheet
+    {
+        public const char Strike = 'X';
+        public const char Spare = '/';
+        public const char Foul = 'F';
+        public const char Miss = '-';
+
+
+    }
+}
--- BowlingTests/GameTests.cs
+++ BowlingTests/GameTests.cs
@@ -5,10 +5,16 @@ namespace BowlingTests
 {
     public class GameTests
     {
+        const string perfectGame = "X X X X X X X X X X X X";
+        const string cleanSheetFiveFive = "5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/5";
+        const string dutchTwoHundred = "5/ X 5/ X 5/ X 5/ X 5/ X 5/5";
+        const string allOpenNines = "9- 9- 9- 9- 9- 9- 9- 9- 9- 9-";
+
         [Theory]
-        [InlineData("X X X X X X X X X X X X", 300)]
-        [InlineData("9- 9- 9- 9- 9- 9- 9- 9- 9- 9-", 90)]
-        [InlineData("5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/5", 150)]
+        [InlineData(perfectGame, 300)]
+        [InlineData(allOpenNines, 90)]
+        [InlineData(cleanSheetFiveFive, 150)]
+        [InlineData(dutchTwoHundred, 200)]
         public void TotalScore(string scoreSheet, int totalScore)
         {
             throw (new NotImplementedException());
