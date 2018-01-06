74b3b13eeb4b6e34adbd27ee49d2816150693a37
gothandy <gothandy@hotmail.com>
2018-01-06 13:37:25 +0000

    Should of started with the IsOpenFrame.

--- BowlingTests/FrameTests.cs
+++ BowlingTests/FrameTests.cs
@@ -6,6 +6,15 @@ namespace BowlingTests
     public class FrameTests
     {
         [Theory]
+        [InlineData(5, 3)]
+        public void IsOpenFrame(params int[] pins)
+        {
+            Frame frame = new Frame(pins);
+
+            Assert.True(frame.IsOpenFrame);
+        }
+
+        [Theory]
         [InlineData(8, 5, 3)]
         public void OpenFrameScore(int expectedScore, params int[] pins)
         {
