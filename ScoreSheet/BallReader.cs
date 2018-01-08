cf2848db320de103c48efe5b40691bf653f38cd7
gothandy <gothandy@hotmail.com>
2018-01-08 13:55:02 +0000

    Refactored.

--- ScoreSheet/BallReader.cs
+++ ScoreSheet/BallReader.cs
@@ -5,26 +5,20 @@ namespace ScoreSheet
 {
     public static class BallReader
     {
-        private const char Miss = '-';
-        private const char Foul = 'F';
-        private const char Spare = '/';
-        private const char Strike = 'X';
-
-        private static char[] list = new char[] { Miss, Foul };
-
         public static int GetPins(char c)
         {
-            if (list.Contains<char>(c)) return 0;
-
+            if (IsMiss(c) || IsFoul(c)) return 0;
             if (IsStrike(c)) return 10;
+            if (IsDigit(c)) return Convert.ToInt32(c.ToString());
 
-            return Convert.ToInt32(c.ToString());
+            throw new NotImplementedException();
         }
+        public static bool IsValid(char c) => (IsDigit(c) || IsMiss(c) || IsFoul(c));
 
-        public static bool IsValid(char c) => list.Contains<char>(c) || char.IsDigit(c);
-
-        public static bool IsStrike(char c) => c == Strike;
-        public static bool IsSpare(char c) => c == Spare;
-
+        public static bool IsDigit(char c) => char.IsDigit(c);
+        public static bool IsStrike(char c) => (c == 'X');
+        public static bool IsSpare(char c) => (c == '/');
+        public static bool IsMiss(char c) => (c == '-');
+        public static bool IsFoul(char c) => (c == 'F');
     }
 }
--- ScoreSheet/FinalSpareReader.cs
+++ ScoreSheet/FinalSpareReader.cs
@@ -4,12 +4,10 @@ namespace ScoreSheet
 {
     public class FinalSpareReader : IFrameReader
     {
-        public BaseFrame GetFrame(string s)
-        {
-            return new FinalSpare(
+        public BaseFrame GetFrame(string s) => new FinalSpare(
                 BallReader.GetPins(s[0]),
-                BallReader.GetPins(s[2]));
-        }
+                BallReader.GetPins(s[2])
+            );
 
         public bool IsMatch(string s) => s.Length == 3 && BallReader.IsSpare(s[1]);
 
