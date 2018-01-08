c533e215032d98b0db70ff82426a4012060a1bf8
gothandy <gothandy@hotmail.com>
2018-01-08 18:14:22 +0000

    Some Frame unit tests.

---
+++ BowlingTests/FrameTests.cs
@@ -0,0 +1,84 @@
+﻿using Bowling;
+using System.Collections.Generic;
+using Xunit;
+
+namespace BowlingTests
+{
+    public class FrameTests
+    {
+        [Fact]
+        public void OpenTest()
+        {
+            List<int> balls = new List<int>();
+
+            Open frame = new Open(1, 1);
+
+            frame.AddBalls(ref balls);
+
+            Assert.Equal(2, frame.Score);
+            Assert.Equal<int>(new int[] { 1, 1 }, frame.Balls);
+            Assert.Equal<int>(new int[] { 1, 1 }, balls);
+        }
+
+        [Fact]
+        public void SpareTest()
+        {
+            List<int> balls = new List<int>();
+
+            Spare frame = new Spare(1);
+
+            frame.AddBalls(ref balls);
+
+            balls.Add(2);
+
+            Assert.Equal(12, frame.Score);
+            Assert.Equal<int>(new int[] { 1, 9 }, frame.Balls);
+            Assert.Equal<int>(new int[] { 1, 9, 2 }, balls);
+        }
+
+        [Fact]
+        public void StrikeTest()
+        {
+            List<int> balls = new List<int>();
+
+            Strike frame = new Strike();
+
+            frame.AddBalls(ref balls);
+
+            balls.Add(2);
+            balls.Add(3);
+
+            Assert.Equal(15, frame.Score);
+            Assert.Equal<int>(new int[] { 10 }, frame.Balls);
+            Assert.Equal<int>(new int[] { 10, 2, 3 }, balls);
+        }
+
+        [Fact]
+        public void FinalSpareTest()
+        {
+            List<int> balls = new List<int>();
+
+            FinalSpare frame = new FinalSpare(1, 5);
+
+            frame.AddBalls(ref balls);
+
+            Assert.Equal(15, frame.Score);
+            Assert.Equal<int>(new int[] { 1, 9, 5 }, frame.Balls);
+            Assert.Equal<int>(new int[] { 1, 9, 5 }, balls);
+        }
+
+        [Fact]
+        public void FinalStrikeTest()
+        {
+            List<int> balls = new List<int>();
+
+            FinalStrike frame = new FinalStrike(2, 3);
+
+            frame.AddBalls(ref balls);
+
+            Assert.Equal(15, frame.Score);
+            Assert.Equal<int>(new int[] { 10, 2, 3 }, frame.Balls);
+            Assert.Equal<int>(new int[] { 10, 2, 3 }, balls);
+        }
+    }
+}
---
+++ ScoreSheetTests/FrameReaderTests.cs
@@ -0,0 +1,31 @@
+﻿using Bowling;
+using ScoreSheet;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace ScoreSheetTests
+{
+    public class FrameReaderTests
+    {
+        [Theory]
+        [InlineData("X", typeof(Strike), 10)]
+        [InlineData("5/", typeof(Spare), 5, 5)]
+        [InlineData("45", typeof(Open), 4, 5)]
+        [InlineData("-/", typeof(Spare), 0, 10)]
+        [InlineData("F9", typeof(Open), 0, 9)]
+        [InlineData("5/5", typeof(FinalSpare), 5, 5, 5)]
+        [InlineData("X23", typeof(FinalStrike), 10, 2, 3)]
+        [InlineData("XX3", typeof(FinalStrike), 10, 10, 3)]
+        [InlineData("XXX", typeof(FinalStrike), 10, 10, 10)]
+        [InlineData("5/X", typeof(FinalSpare), 5, 5, 10)]
+        public void GetFrameReader(string s, Type t, params int[] p)
+        {
+            BaseFrame frame = GameReader.GetFrame(s);
+
+            Assert.Equal(t, frame.GetType());
+            Assert.Equal<int>(p, frame.Balls);
+        }
+    }
+}
